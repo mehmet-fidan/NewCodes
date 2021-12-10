ee937d7546bad57d8e4e4fa3d2a9fd0233390e57
mehmet <mfidan21@gmail.com>
2021-12-10 18:10:30 +0300

    day 1

--- src/arrays2D/_01_Array2D.java
+++ src/arrays2D/_01_Array2D.java
@@ -3,10 +3,7 @@ package arrays2D;
 public class _01_Array2D {
     public static void main(String[] args) {
 
-
-
-
-        int[][] nums =new int[4][3];
+   int[][] nums =new int[4][3];
 
 
         for (int i = 0; i < nums.length ; i++) {
