cb05dfcd4638f7ae1786f6383205a5666fc50394
mehmet <mfidan21@gmail.com>
2022-01-11 00:30:22 +0300

    day7

---
+++ src/arrayLists/_05_ArrayListExample.java
@@ -0,0 +1,60 @@
+package arrayLists;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class _05_ArrayListExample {
+    public static void main(String[] args) {
+        /*
+        Array            ArrayList
+        siniri belli     Siniri belli değil
+         */
+
+        String str = " aa ";   //immutable
+        str.trim();
+        System.out.println(str.trim());
+        System.out.println(str);
+        str = str.trim();     // "aa"
+        System.out.println(str);
+
+        int a = 5;
+
+        int[] array = new int[5];
+        int[] array1 = new int[10];
+
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(23);
+        list.add(10);
+        list.add(1234);
+        list.add(-1);
+        list.add(1);
+        System.out.println(list);
+
+        LinkedList<Integer> list1 = new LinkedList<>();
+        list1.add(1);
+        list1.add(2);
+        list1.add(23);
+        list1.add(10);
+        list1.add(1234);
+        list1.add(-1);
+        list1.add(1);
+        System.out.println(list1);
+
+        System.out.println(list.get(3));
+
+        System.out.println("----------");
+
+        for (int i = 0; i < list.size(); i++) {
+            if (i == 4) continue;
+            System.out.print(list.get(i) + "\t");
+        }
+        System.out.println();
+
+        for (Integer i : list) {
+            System.out.print(i + "\t");
+        }
+        System.out.println();
+    }
+}
