c87edd23ffcb362d116d31c611f52cbc88291ee9
mehmet <mfidan21@gmail.com>
2022-01-12 01:40:30 +0300

    day9

---
+++ src/arrayLists/ForEach.java
@@ -0,0 +1,31 @@
+package arrayLists;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+    public class ForEach {
+        public static void main(String[] args) {
+
+            ArrayList<Integer> list = new ArrayList<>(Arrays.asList(1, 2, 3, 7, 8, 12, 35));
+
+            for (int i = 0; i < list.size(); i++) {
+                System.out.print(list.get(i) + ", ");
+            }
+            System.out.println();
+
+            for (Integer integer : list) {
+                System.out.print(integer + ", ");
+            }
+            System.out.println();
+
+            //lambda n -> ......
+            list.forEach(integer -> System.out.print(integer + ", "));
+            System.out.println("****");
+
+        }
+
+        public static void yaz(int integer) {
+            System.out.println(integer + ", ");
+        }
+    }
+
